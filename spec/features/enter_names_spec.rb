c39e0a4cfc35f606f052fbf0bcf5b6b225d38aa8
Erlantz <erlantzram@gmail.com>
2022-05-06 15:41:26 +0100

    step 22 done: using helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,9 @@
 
 feature "Enter names" do
   scenario 'players submit naems' do
-    visit('/')
-    fill_in :player_1_name, with: 'Erlantz'
-    fill_in :player_2_name, with: 'Laura'
-    click_button 'Submit'
 
+    sign_in_and_play
     expect(page).to have_content 'Erlantz vs Laura'
+    
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,6 @@
 feature "Hit points" do
   scenario 'player 2 hit points are shown' do
-    visit('/')
-    fill_in :player_1_name, with: 'Erlantz'
-    fill_in :player_2_name, with: 'Laura'
-    click_button 'Submit'
-
+    sign_in_and_play
     expect(page).to have_content 'Laura: 60HP'
   end
 end
